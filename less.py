e3ff67f9634d8c8c11b9cb167e2e4ec466facc70
Bohdan <s00ndie@icloud.com>
2024-08-17 15:24:49 +0200

    homework2.2

--- less.py
+++ less.py
@@ -1,6 +1,7 @@
-number = int(input("Enter number between 1000 - 9999:"))
-n1 = number // 1000
-n2 = number % 1000 // 100
-n3 = number //10 % 10
-n4 = number % 10
-print(f"{n1} \n{n2} \n{n3} \n{n4}")
+number = int(input("Enter number between 10000 - 99999:"))
+n1 = number // 10000
+n2 = number // 1000 % 10
+n3 = number % 1000 // 100
+n4 = number //10 % 10
+n5 = number % 10
+print(f"{n5}{n4}{n3}{n2}{n1}" )
