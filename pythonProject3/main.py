32648b2a9e9225af931e79a28fd1bbf3966c12e9
Bohdan <s00ndie@icloud.com>
2024-08-24 18:08:33 +0200

    4.1

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -1,10 +1,13 @@
 import random
 list=[]
 for i in range(random.randint(3,10)):
-    list.append(random.randint(1,10))
+    list.append(random.randint(0,10))
 print(list)
-even_elements = sum(list[::2])
-print(even_elements)
-last_char= list[-1]
-print(last_char)
-print(even_elements*last_char)
+non_zero_index=0
+for i in range(len(list)):
+    if list[i] !=0:
+        list[non_zero_index]=list[i]
+        non_zero_index+=1
+for i in range(non_zero_index,len(list)):
+    list[i]=0
+print(list)
