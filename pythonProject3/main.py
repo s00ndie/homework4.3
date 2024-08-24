829b6d9c47933150d52b5be150a550951ae8b154
Bohdan <s00ndie@icloud.com>
2024-08-24 12:10:27 +0200

    4.2

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -3,11 +3,8 @@ list=[]
 for i in range(random.randint(3,10)):
     list.append(random.randint(1,10))
 print(list)
-a= list[:1]
-b= list[2:3]
-c= [list[-2]]
-print(a)
-print(b)
-print(c)
-list2= a+b+c
-print(list2)
+even_elements = sum(list[::2])
+print(even_elements)
+last_char= list[-1]
+print(last_char)
+print(even_elements*last_char)
