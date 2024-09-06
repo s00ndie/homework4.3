ed70181277201a351267ec2316563c74b903b188
Bohdan <s00ndie@icloud.com>
2024-09-06 16:55:38 +0200

    5.1

--- pythonProject4/5.2.py
+++ pythonProject4/5.2.py
@@ -1,9 +1,28 @@
 import string
-text = input('Enter your text:')
-punc = str.maketrans('', '', string.punctuation )
-clean_text = text.translate(punc)
-clean_text= clean_text.title().replace(' ', '')
-if len(clean_text) > 140:
-    clean_text=clean_text[:140]
-print(f'#{clean_text}')
-print(len(clean_text))
+import keyword
+symbols = string.punctuation
+symbols=symbols.replace("_", ' ')
+
+name = input('Enter variable name: ')
+if len(name)>0:
+    if name[0] == '_' or name[0].isalpha() and len(name)==1:
+        print('True')
+    elif name in keyword.kwlist:
+        print(False)
+    elif name[0].isalpha() and name.islower():
+        is_correct = True
+        for letter in name:
+            if letter in symbols:
+                is_correct = False
+                break
+        if is_correct:
+                print(True)
+        else:
+            print(False)
+
+    else:
+        print(False)
+else:
+    print(False)
+
+
